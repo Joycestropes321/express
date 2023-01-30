90e4b1867ddb6f05f20b195736bd8bcc39d495c0
Joycestropes321 <94107763+Joycestropes321@users.noreply.github.com>
2023-01-30 11:20:15 +0100

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
     <a href="#" class="brand-link">
        <img src="{{ asset('dist/img/logo.png') }}" class="brand-image img-circle elevation-3"  
            style="opacity: .8">
-      <span class="brand-text font-weight-light">Express Union</span>
+      <span class="brand-text font-weight-light">Wellsfago</span>
     </a>
 
     <!-- Sidebar -->
