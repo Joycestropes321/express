c29c0b1b9da946ab4ce0ef25b359c95985aed28f
Joycestropes321 <94107763+Joycestropes321@users.noreply.github.com>
2023-02-20 06:39:53 +0100

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8"> 
-    <title>App Name - @yield('title')</title> 
+    <title>App - @yield('title')</title> 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   
